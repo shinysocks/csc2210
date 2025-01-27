68de3904a258df63a57e50c0e50e16044c267a0a
Noah Dinan <noah@shinysocks.net>
2025-01-27 13:27:51 -0600

    add initial exercises

---
+++ exercise1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "The cube of ";
+    cout << n;
+    cout << " is ";
+    cout << n * n;
+    return 0;
+}
+
