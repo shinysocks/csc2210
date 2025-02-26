cac71ca5f7c93df663dd57b56faf3e4a00807248
Noah Dinan <noah@shinysocks.net>
2025-02-26 17:41:04 -0600

    complete spa2

--- spa2/2word.cpp
+++ spa2/2word.cpp
@@ -29,7 +29,7 @@ bool prune_word(string guess, string pattern, string word) {
                 if (guess[i] != word[i]) return true; 
                 break;
             case '?':
-                if (word.find(guess[i]) == string::npos) return true;
+                if (word[i] == guess[i] || word.find(guess[i]) == string::npos) return true;
                 break;
             default:
                 if (word.find(guess[i]) != string::npos) return true;
@@ -38,21 +38,26 @@ bool prune_word(string guess, string pattern, string word) {
     } return false;
 }
 
-void process_guesses(string guesses[], string words[], int size) {
+int process_guesses(string guesses[], string words[], int size) {
     string pattern;
     int i = 0;
+    cin >> guesses[i];
+    cin >> pattern;
+
+    if (!cin) return 1;
 
     while (cin) {
-        cin >> guesses[i];
-        cin >> pattern;
-
         for (int j = 0; j < size; j++) {
-            if (prune_word(guesses[i], pattern, words[j])) {
+            if (words[j] != "" && guesses[i] != "" && prune_word(guesses[i], pattern, words[j])) {
                 words[j] = "";
             }
         }
+
         i++;
+        cin >> guesses[i];
+        cin >> pattern;
     }
+    return 0;
 }
 
 int count(string strs[]) {
@@ -62,22 +67,25 @@ int count(string strs[]) {
     } return c;
 }
 
+
 int main() {
     string words[MAX_SIZE];
     string guesses[MAX_SIZE];
     int words_size = populate_words(words);
-    process_guesses(guesses, words, words_size);
-    int guess_size = count(guesses);
+    int guess_size = 1;
+    if (process_guesses(guesses, words, words_size) == 1) goto end;
+    guess_size = count(guesses);
     words_size = count(words);
 
-    cout << "Possible guesses after ";
-    for (int i = 0; i < guess_size - 1; i++) {
-        cout << guesses[i] + ", ";
-    } cout << guesses[guess_size - 1] << ": " << words_size << endl;
+    end:
+        cout << "Possible guesses after ";
+        for (int i = 0; i < guess_size - 1; i++) {
+            cout << guesses[i] + ", ";
+        } cout << guesses[guess_size - 1] << ": " << words_size << endl;
 
-    for (int i = 0; i < MAX_SIZE; i++) {
-        cout << ((words[i] == "") ? "" : words[i] + "\n");
-    }
-
+        for (int i = 0; i < MAX_SIZE; i++) {
+            cout << ((words[i] == "") ? "" : words[i] + "\n");
+        }
     return 0;
 }
+
